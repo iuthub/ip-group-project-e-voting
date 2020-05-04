0d4a442f56c9fa43c72b0fbb9970f74f3e8e6be8
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2020-05-04 18:58:12 +0500

    dashboard-posts,category,users created

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	document.getElementById("sysdate").value = new Date();
+}
+// Menu Toggle Script
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
